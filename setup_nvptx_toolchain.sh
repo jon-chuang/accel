2a7a9a152c1d9b2092024719bdf01f54a2ef08a2
jonch <9093549+jon-chuang@users.noreply.github.com>
2020-11-08 11:44:12 +0800

    update setup script

--- setup_nvptx_toolchain.sh
+++ setup_nvptx_toolchain.sh
@@ -3,5 +3,6 @@ set -xue
 
 NIGHTLY=nightly-2020-09-20
 rustup toolchain add ${NIGHTLY}
+rustup component add rustfmt --toolchain ${NIGHTLY}
 rustup target add nvptx64-nvidia-cuda --toolchain ${NIGHTLY}
 cargo install ptx-linker -f
